8dca5987b3906d0cc57170765d795c9b4b92f3dc
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 19:46:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   empty_array = []
   counter = 0
   while counter < array.length do
-    empty_array << ield(languages[counter])
+    empty_array << yield(languages[counter])
     counter += 1
   end
   empty_array
