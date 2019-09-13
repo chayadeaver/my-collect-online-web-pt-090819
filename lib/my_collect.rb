becea0d4f8b37aed5c350859c9d62779b1e6bb92
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 19:21:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,11 +2,10 @@
 def my_collect(array)
   empty_array = []
   counter = 0
-  while counter < array.length do
-    yield
+  my_collect(empty_array) do |x|
     counter += 1
   end
 empty_array
 end
 
-my_collect(empty_array) do |x|
+
