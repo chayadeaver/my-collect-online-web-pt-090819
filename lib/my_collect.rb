120cf4c13c4b7c224bde01e0ac030c7ba752bfe8
Chaya Deaver <chayadeaver@gmail.com>
2019-09-13 19:54:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,6 @@ def my_collect(array)
   counter = 0
   while counter < array.length do
     empty_array << yield(students[counter]
-    yield if block_given?(languages[counter])
     counter += 1
   end
   empty_array
